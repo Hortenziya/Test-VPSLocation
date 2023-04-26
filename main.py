ef2af34bffcaec97582f2bcc168712ee4eb43db9
anastasiaceban <nastcheban@gmail.com>
2023-04-26 10:33:03 +0300

    added SEORS

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, request, send_from_directory
 from location import Location, Host
 from replication import Replication
 from upload import FileUpload
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 servers = [
     {
